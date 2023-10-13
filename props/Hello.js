428788822c0dced041065555d2d74218ca7b8895
MZ01-LIKE1011 <mz01-like1011@cpe-172-90-3-180.socal.res.rr.com>
2023-10-13 15:18:32 +0900

    to commit

--- props/Hello.js
+++ props/Hello.js
@@ -5,6 +5,7 @@ export default function Hello({ target, target2 }) {
     <>
       <div>Hello React {target}</div>
       <div>Hello React {target2}</div>
+      <p>깃 업데이트 되는건가</p>
     </>
   );
 }
