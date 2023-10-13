c256d4f105ced47e9c27aad92e80f060f0bd4515
MZ01-LIKE1011 <mz01-like1011@cpe-172-90-3-180.socal.res.rr.com>
2023-10-13 15:30:25 +0900

    수정연습

--- props/Hello.js
+++ props/Hello.js
@@ -6,6 +6,7 @@ export default function Hello({ target, target2 }) {
       <div>Hello React {target}</div>
       <div>Hello React {target2}</div>
       <p>깃 업데이트 되는건가</p>
+      <p>진짜로 정말 되는거지?</p>
     </>
   );
 }
